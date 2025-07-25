bca04971d289915dc8a6fcb4f7e1b3f39c2e6ef6
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-07-25 02:34:37 +0100

    made an update that add 'self.name' to author model django-model/libraryproject

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,6 +8,8 @@ with this , if we delete an Author, we automatically delete all their books as w
 """
 class Author(models.Model):
     name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
