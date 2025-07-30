4e5567fdea5e242887d24b4ee155f71dc80e9798
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-07-29 19:56:54 -0600

    made an  update that added automatic signal to create user profile

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,8 @@ from django.views.generic import DetailView, ListView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
-from django.views.generic.detail import DetailView
+
+from django.contrib.auth import login
 
 
 
