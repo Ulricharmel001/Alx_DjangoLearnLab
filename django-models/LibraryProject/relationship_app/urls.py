0852f13f132a35d6ec1531eb68fda8a1fd29ad82
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-07-29 20:07:05 -0600

    made an  update that added automatic signal to create user profile

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import SignupView, LoginView, LogoutView, list_books, LibraryDetailView
 from .views import list_books, register
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     # Signup using your custom view
-    path('register/', register.as_view(), name='register'),
+    path('register/', views.register name='register'),
     # Login and Logout using Django built-in auth views
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout'), name='logout'),
