10a48d1bd8085f1b60afbe0215dbbf458bc0a9c8
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-07-29 21:56:35 -0600

    added role base access features

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -110,3 +110,11 @@ def is_member(user):
 def member_view(request):
     return render(request, 'member_view.html')
 
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
