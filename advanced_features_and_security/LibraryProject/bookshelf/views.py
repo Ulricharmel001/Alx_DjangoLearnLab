dd0014dfe3db4b15f3aaadd0fd982d3cfacc0688
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-08-08 05:43:00 +0100

    added features for securing django app from view to setting file, even forms

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required, login_required
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 
 # View to list all books — no permission needed to view, or you can add 'can_view'
@@ -11,33 +11,36 @@ def book_list(request):
 
 
 # View to create a new book (requires can_create permission)
+@login_required
 @permission_required('books.can_create', raise_exception=True)
 def create_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/create_book.html', {'form': form})
 
 
 # View to edit a book (requires can_edit permission)
+@login_required
 @permission_required('books.can_edit', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm(instance=book)
+        form = ExampleForm(instance=book)
     return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
 
 
 # View to delete a book (requires can_delete permission)
+@login_required
 @permission_required('books.can_delete', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
