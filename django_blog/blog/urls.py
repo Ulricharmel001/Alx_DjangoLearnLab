7dbb24cb9cbbb19e209425720c1b1f92a19873fd
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-08-30 17:22:50 +0100

    added comment crud operation view and template with urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Posts
     path('', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
@@ -16,7 +17,7 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
 
     # Comments
-    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),
-    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
-    path('posts/<int:post_id>/comments/new/',views.CommentCreateView.as_view(),name='add_comment'
-),]
+    path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+]
